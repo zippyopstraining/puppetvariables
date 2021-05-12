43c52196c41af560d08541f33e5496fe0afc2edf
zippyopstraining <77266301+zippyopstraining@users.noreply.github.com>
2021-05-12 13:05:16 +0530

    Add files via upload

---
+++ scoped-variable.pp
@@ -0,0 +1,33 @@
+# @summary Variables and scope
+#
+# Variables and scope
+#
+# @example
+#   include my_variables::scoped_variables
+class my_variables::scoped_variables {
+  $my_content = 'Hello Scoped'
+
+  file { '/tmp/scoped_variables_0':
+    ensure  => 'present',
+    content => $my_content,
+    path    => '/tmp/scoped_variables_0',
+  }
+
+  file { '/tmp/scoped_variables_1':
+    ensure  => 'present',
+    content => $my_variables::local_variables::my_content,
+    path    => '/tmp/scoped_variables_1',
+  }
+
+  file { '/tmp/scoped_variables_2':
+    ensure  => 'present',
+    content => $my_variables::my_content,
+    path    => '/tmp/scoped_variables_2',
+  }
+
+  file { '/tmp/scoped_variables_3':
+    ensure  => 'present',
+    content => $::my_content,
+    path    => '/tmp/scoped_variables_3',
+  }
+}
---
+++ site.pp
@@ -0,0 +1,7 @@
+$my_content = 'Hello Top'
+
+node default {
+  include my_module
+  include my_variables
+}
+}
